470467c13a1f31d4333c1f6afb4bc643a2a1a61a
Kaic de Lima Oliveira <kaic10@outlook.com>
2022-01-10 14:18:07 -0300

    commit

--- routes/loginRoutes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request
-
-login = Blueprint('login', __name__, template_folder='templates', url_prefix='/login')
-
-@login.route('')
-def index():
-    return '<h1>Login</h1>'
